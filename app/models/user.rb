7b335b27f2d869f4c01b13b224ca270f5af69b2f
Moritz Metelmann <moritz@metelmann.biz>
2022-05-30 17:48:02 +0200

    Created user model and added name migration to user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Also available: user_signed_in? and current_user
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
