e1f67567fc9d69a6c85b557f75f1be9cdf0a363d
Moritz Metelmann <moritz@metelmann.biz>
2022-06-01 14:25:12 +0200

    Made answers fully work. Open for ideas tho

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,12 @@
 class AnswersController < ApplicationController
-  def new
-
-  end
-
-  def create
-
-  end
-
   def destroy
-
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    poll = @answer.poll
+    if poll.project_id.nil?
+      redirect_to polls_path
+    else
+      redirect_to project_path(poll.project_id)
+    end
   end
 end
