fe8cc798420d169b8d0182824cf6d55e8cf1c258
Moritz Metelmann <moritz@metelmann.biz>
2022-06-01 10:28:07 +0200

    Routing for logged in fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  authenticated :user do
+    root to: 'projects#index'
+  end
+  unauthenticated :user do
+    root to: 'pages#home', as: :unauthenticated_root
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :users, only: [:show]
   resources :projects
