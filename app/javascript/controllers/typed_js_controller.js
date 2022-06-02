28a8830b41c98f721f7e6634aa8f756ac7ce1d4b
manuela toro <manuelatoro311@gmail.com>
2022-06-02 16:41:52 +0200

    seeds

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
-import Typed from "typed.js"
+// import Typed from "typed.js"
 
 export default class extends Controller {
 
   connect() {
-    new Typed(this.element, {
-      strings: ["DEAL."],
-      typeSpeed: 60,
-      loop: true
-    });
+//     new Typed(this.element, {
+//       strings: ["DEAL."],
+//       typeSpeed: 60,
+//       loop: true
+//     });
   }
 }
