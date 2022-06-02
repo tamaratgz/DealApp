f03e93b3a83b1988a61dedaa8f0f61488e9e6f48
Moritz Metelmann <moritz@metelmann.biz>
2022-06-02 10:13:45 +0200

    Made voting function work. Missing: only one vote per poll

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,16 @@
 class VotesController < ApplicationController
   def create
-
+    @vote = Vote.new
+    @vote.answer = Answer.find(params[:answer_id])
+    @vote.user = current_user
+    if @vote.save
+      if @vote.answer.poll.project_id.nil?
+        redirect_to polls_path
+      else
+        redirect_to project_path(@vote.answer.poll.project_id)
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
