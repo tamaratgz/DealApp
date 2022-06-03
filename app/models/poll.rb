c39151438e0abef4c0f0a7457b32fa25bc7db866
Moritz Metelmann <moritz@metelmann.biz>
2022-06-03 12:36:03 +0200

    voting feature works

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,6 +2,7 @@ class Poll < ApplicationRecord
   belongs_to :user
   belongs_to :project, optional: true
   has_many :answers, dependent: :destroy
+  has_many :votes, through: :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,18 @@
 class Vote < ApplicationRecord
   belongs_to :answer
   belongs_to :user, optional: true
+
+  validate :one_vote_per_poll, :deadline_valid
+
+  def one_vote_per_poll
+    unless answer.poll.votes.where(user_id: self.user_id).empty?
+      errors.add(:one_vote_per_poll, "You can only vote once")
+    end
+  end
+
+  def deadline_valid
+    if (answer.poll.deadline - Date.current) < 0
+      errors.add(:deadline_valid, "Poll has ended")
+    end
+  end
 end
